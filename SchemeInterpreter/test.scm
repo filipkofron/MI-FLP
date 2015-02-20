e7cdc0d629c2510b92b6ddd053abeed11b98eb8a
Filip Kofron <kofee@lt>
2015-02-20 11:18:28 +0100

    Implemented parsing Scheme.

--- SchemeInterpreter/test.scm
+++ SchemeInterpreter/test.scm
@@ -1,10 +1,21 @@
 (define
 	fac (lambda (a)
 	(
-		(if (eq a 0)
+		if (eq? a 0)
 			1
-			(* (fac (- a 1)) a)
-		)
+			(* a (fac (- a 1))
+			)
 	)
-)
+))
+
+(define
+	fib (lambda (a)
+	(
+		if (< a 3)
+			1
+			(+ (fib (- a 2)) (fib (- a 1)))
+	)
+))
+
+(print "Hello, world")
 
