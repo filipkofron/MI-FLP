9971ce8beea5fbea652a10b2972e8f59706921ef
Filip Kofron <filip.kofron.cz@gmail.com>
2015-03-31 12:15:30 +0200

    Fixed evaluating with errors.

--- VM/vm.lisp
+++ VM/vm.lisp
@@ -1,8 +1,17 @@
+(defun read-char-safe ()
+    (let ((c (read-char *standard-input* NIL 'eof)))
+        (if (eq c 'eof)
+            (exit)
+            c
+        )
+    )
+)
+
 (let ((a NIL))
 	(defun vm-peek-char ()
 		(if a
 			a
-			(setf a (read-char))
+                        (setf a (read-char-safe))
 		)
 	)
 	(defun vm-read-char ()
@@ -13,7 +22,7 @@
 					ret
 				)
 			)
-			(read-char)
+                        (read-char-safe)
 		)
 	)				
 )
@@ -33,8 +42,12 @@
 
 (defun vm-read-token ()
 	(cond
-		((eq (vm-peek-char) #\linefeed)
-			(cons NIL NIL))
+                ((eq (vm-peek-char) #\linefeed)
+                    (progn
+                        (vm-read-char)
+                        (cons NIL NIL)
+                    )
+                )
 		((digit-char-p (vm-peek-char))
 			(cons (vm-read-num) NIL))
 		((eq (vm-peek-char) #\+)
@@ -45,16 +58,123 @@
 			(cons (char-to-str (vm-read-char)) NIL))
 		((eq (vm-peek-char) #\))
 			(cons (char-to-str (vm-read-char)) NIL))
+                (1
+                    (progn
+                        (vm-read-char)
+                        NIL
+                    )
+                )
 	)
 )
 
-(defun vm-eval ()
-	(let ((token (vm-read-token)))
-		(cond
-			((eq token #\() (vm-eval))
-			(1 NIL))
-	)
+(defun vm-eval-lvalue-rest (lval level)
+    (let ((token (vm-read-token)))
+        (cond
+            ((eq token NIL)
+                (error-read-until-newline)
+            )
+            ((eq (car token) NIL)
+                (if (equal level 0)
+                    lval
+                    (error-now))
+            )
+            ((string-equal (car token) ")")
+                (if (equal level 0)
+                    (error-read-until-newline)
+                    lval
+                )
+            )
+            ((string-equal (car token) "+")
+                (let ((res (vm-eval level)))
+                    (if (stringp res)
+                        res
+                        (+ lval res)
+                    )
+                )
+            )
+            ((string-equal (car token) "*")
+                (let ((res (vm-eval-single-token level)))
+                    (if (stringp res)
+                        res
+                        (vm-eval-lvalue-rest (* lval res) level)
+                    )
+                )
+            )
+            (T (error-read-until-newline))
+        )
+    )
 )
 
-(print (vm-eval))
+(defun vm-eval-single-token (level)
+    (let ((token (vm-read-token)))
+            (cond
+                    ((eq token NIL)
+                        (error-read-until-newline)
+                    )
+                    ((eq (car token) NIL)
+                        (error-now)
+                    )
+                    ((or (string-equal (car token) "+") (string-equal (car token)  "*") (string-equal (car token) ")"))
+                        (error-read-until-newline)
+                    )
+                    ((string-equal (car token) "(") (vm-eval (+ level 1)))
+                    (T (parse-integer (car token)))
+            )
+    )
+)
 
+(defun error-now ()
+    "ERROR"
+)
+
+(defun error-read-until-newline ()
+    (let ((token (vm-read-token)))
+        (if (eq token NIL)
+            (error-read-until-newline)
+            (if (eq (car token) NIL)
+                "ERROR"
+                (error-read-until-newline)
+            )
+        )
+    )
+)
+
+(defun vm-eval (level)
+        (let ((token (vm-read-token)))
+                (cond
+                        ((eq token NIL)
+                            (error-read-until-newline)
+                        )
+                        ((eq (car token) NIL)
+                            (error-now)
+                        )
+                        ((or (string-equal (car token) "+") (string-equal (car token)  "*") (string-equal (car token) ")"))
+                            (error-read-until-newline)
+                        )
+                        ((string-equal (car token) "(")
+                            (let ((res (vm-eval (+ level 1))))
+                                (if (stringp res)
+                                    res
+                                    (vm-eval-lvalue-rest
+                                        res
+                                        level
+                                    )
+                                )
+                            )
+                        )
+                        (T (vm-eval-lvalue-rest (parse-integer (car token)) level))
+                )
+        )
+)
+
+(defvar n (parse-integer (vm-read-num)))
+(vm-read-char)
+
+(defun vm-run-for (a)
+    (when (> a 0)
+        (vm-run-for (- a 1))
+        (print (vm-eval 0))
+    )
+)
+
+(vm-run-for n)
