177a8f8c00ec4c0f11dc66c2fe3dc668b205c8af
Filip Kofron <filip.kofron.cz@gmail.com>
2015-05-05 10:10:32 +0200

    Added solution with broken encloses.

--- VM/sem.pl
+++ VM/sem.pl
@@ -1,35 +1,26 @@
-op_sum(OP) :- OP = "+".
-op_mul(OP) :- OP = "*".
+sum_op(["+"]).
+mul_op(["*"]).
+open_op(["("]).
+close_op([")"]).
 
-op(OP) :- op_mul(OP), !.
-op(OP) :- op_sum(OP).
+nempty(L) :- length(L, LenL), LenL > 0.
+prefix(P, C, L) :- length(L, LenL), LenL > 1, nempty(P), nempty(C), nempty(L), append(P, C, L).
+suffix(S, C, L) :- length(L, LenL), LenL > 1, nempty(S), nempty(C), nempty(L), append(C, S, L).
 
-bin_e(L, OP, R, E) :- expr(E), op(OP), expr(L), expr(R).
+encloses([L], [V], [R], List) :- List = [L, V, R].
+encloses(L, V, R, List) :- length(List, LenList), LenList > 2, nempty(L), nempty(V), nempty(R), length(L, LenL), LenL < LenList, prefix(L, V, C), length(C, LenC), LenC < LenList, length(R, LenR), LenR < LenList, suffix(R, C, L).
 
-expr(E) :- integer(E).
+op(OP) :- mul_op(OP).
+op(OP) :- sum_op(OP).
 
-par_e(E, IE) :-
-	expr(E),
-	expr(IE).
+bin_op(L, OP, R, List) :- encloses(L, OP, R, List), op(OP).
+par(P, E) :- encloses(L, E, R, P), open_op(L), close_op(R).
 
-eval_e(E, RES) :-
-	integer(E),
-	RES is E.
+expression(E, Res) :- bin_op(L, OP, R, E), sum_op(OP), !, component(L, Res1), expression(R, Res2), Res is Res1 + Res2.
+expression(E, Res) :- component(E, Res).
 
-eval_e(E, RES) :-
-	par_e(E, IE),
-	eval_e(IE, RES).
+component(C, Res) :- bin_op(L, OP, R, C), mul_op(OP), factor(L, Res1), component(R, Res2), Res is Res1 * Res2.
+component(C, Res) :- factor(C, Res).
 
-eval_e(E, RES) :- 
-	bin_e(L, OP, R, E),
-	eval_e(L, RES1),
-	eval_e(R, RES2),
-	op_sum(OP),
-	RES is RES1 + RES2.
-
-eval_e(E, RES) :-
-	bin_e(L, OP, R, E),
-	eval_e(L, RES1),
-	eval_e(R, RES2),
-	op_mul(OP),
-	RES is RES1 * RES2.
+factor([Res], Res) :- integer(Res).
+factor([F|Rest], Res) :- open_op([F]), length(Rest, Len1), Len1 > 1, par([F | Rest], E), expression(E, Res).
